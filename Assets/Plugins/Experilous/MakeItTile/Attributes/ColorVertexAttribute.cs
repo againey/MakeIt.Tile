e7b9c0bf588353c86796564e65f73ec4db0b3482
Andy Gainey <7810015+againey@users.noreply.github.com>
2016-10-17 22:27:18 -0500

    Documented the concrete vertex/edge/face attribute collection types.

--- Assets/Plugins/Experilous/MakeItTile/Attributes/ColorVertexAttribute.cs
+++ Assets/Plugins/Experilous/MakeItTile/Attributes/ColorVertexAttribute.cs
@@ -6,9 +6,27 @@ using UnityEngine;
 
 namespace Experilous.MakeItTile
 {
+	/// <summary>
+	/// A concrete <see cref="Color"/>-typed vertex attributes collection, derived from <see cref="ScriptableObject"/>
+	/// and can therefore be serialized, referenced, and turned into an asset by Unity.
+	/// </summary>
+	/// <remarks><para>Because Unity will serialize neither intervertex nor generic type,
+	/// this and other attribute classes drived from <see cref="ScriptableObject"/> are
+	/// necessary for native serialization.</para></remarks>
 	public class ColorVertexAttribute : VertexArrayAttribute<Color>
 	{
+		/// <summary>
+		/// Creates a <see cref="Color"/>-typed vertex attributes collection using the given array as the vertex attribute values.
+		/// </summary>
+		/// <param name="array">The array of vertex attribute values which will be stored, by reference, within the created vertex attributes collection.</param>
+		/// <returns>A <see cref="Color"/>-typed vertex attributes collection.</returns>
 		public static ColorVertexAttribute Create(Color[] array) { return CreateDerived<ColorVertexAttribute>(array); }
+
+		/// <summary>
+		/// Creates a <see cref="Color"/>-typed vertex attributes collection, allocating an array suitable for the given number of vertices.
+		/// </summary>
+		/// <param name="vertexCount">The number of vertices for which the vertex attribute collection should have sufficient storage.</param>
+		/// <returns>A <see cref="Color"/>-typed vertex attributes collection.</returns>
 		public static ColorVertexAttribute Create(int vertexCount) { return CreateDerived<ColorVertexAttribute>(vertexCount); }
 	}
 }
--- Assets/Plugins/Experilous/MakeItTile/Attributes/ConstantColorFaceAttribute.cs
+++ Assets/Plugins/Experilous/MakeItTile/Attributes/ConstantColorFaceAttribute.cs
@@ -6,8 +6,21 @@ using UnityEngine;
 
 namespace Experilous.MakeItTile
 {
+	/// <summary>
+	/// A concrete <see cref="Color"/>-typed face attributes collection which returns
+	/// the same constant value for all faces, derived from <see cref="ScriptableObject"/>
+	/// and can therefore be serialized, referenced, and turned into an asset by Unity.
+	/// </summary>
+	/// <remarks><para>Because Unity will serialize neither interface nor generic type,
+	/// this and other attribute classes drived from <see cref="ScriptableObject"/> are
+	/// necessary for native serialization.</para></remarks>
 	public class ConstantColorFaceAttribute : FaceConstantAttribute<Color>
 	{
+		/// <summary>
+		/// Creates a <see cref="Color"/>-typed face attributes collection using the given value as the shared constant facea attribute value.
+		/// </summary>
+		/// <param name="constant">The value to use as the shared constant facea attribute value.</param>
+		/// <returns>A <see cref="Color"/>-typed constant face attributes collection.</returns>
 		public static ConstantColorFaceAttribute Create(Color constant) { return CreateDerived<ConstantColorFaceAttribute>(constant); }
 	}
 }
